ede2a5d4ce1d9c0305762ce6791e8705dbf19e2a
MARS77662 <d477188@gmail.com>
2025-08-08 21:13:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,4 @@ const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.json$/,
-      type: 'asset/resource',
-      include: path.resolve(__dirname, 'public/.well-known'),
-      generator: {
-        filename: '.well-known/[name][ext]',
-      },
-    })
-    return config
-  },
 }
